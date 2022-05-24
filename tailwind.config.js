3b4c21a34f69a42bb3fcedbd758d6aae106540e6
dseum22 <dseum22@gmail.com>
2022-05-24 14:18:42 -0500

    Working tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ['Nunito', 'sans-serif'],
+            serif: ['PlayfairDisplay', 'serif'],
+        },
+        extend: {
+            colors: {
+                primary: {
+                    100: '#fdffbc',
+                    200: '#ffeebb',
+                    300: '#ffdcb8',
+                    400: '#ffc1b6',
+                    active: '#fcd34d',
+                    hover: '#fde68a',
+                },
+            },
+            backgroundImage: theme => ({
+                'image-home': "url('../public/images/fillers/home.png')",
+                'image-about': "url('../public/images/fillers/about.jpg')",
+                'image-enroll': "url('../public/images/fillers/enroll.jpg')",
+            }),
+        },
     },
     plugins: [],
 }
